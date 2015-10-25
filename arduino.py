2784af3a145961e7351d6727ca87f88998d02a38
Alex Campbell <alex@alexthejourno.com>
2015-10-24 20:10:25 -0700

    Added logging to CSV

--- arduino.py
+++ arduino.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python2
 
-import serial, binascii
+import serial;
+import binascii;
+import time;
+import io;
 
 # Check device TTY and baud rate before deployment to ensure match with Arduino connection
 arduino = serial.Serial('/dev/ttyACM0', 9600)
@@ -27,12 +30,54 @@ def read_arduino():
 #	arduino.write(cmdstr)
 	
 
-#Test output	
-while True:
-	data = read_arduino()
-	if data != False :
-		for k,v in data.items():
-			print (k, " -- ", v)
+#Test output
+def arduino_test():
+	while True:
+		data = read_arduino()
+		if data != False :
+			for k,v in data.items():
+				print (k, " -- ", v)
+				
+def arduino_log():
+	out = 'timestamp,temperature,humidity,pressure,pTemperature,accelX,accelY,accelZ,magX,magY,magZ,latitude,longitude,altitude,speed\n'
+	start = time.time()
+	while time.time() - start < 60 :
+		failures = 0
+		line = ''
+		data = read_arduino()
+		stamp = time.ctime()
+		if data == False :
+			line += 'NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL'
+		else :
+			keys = ['t', 'h', 'p', 'pt', 'ax', 'ay', 'az', 'mx', 'my', 'mz', 'lat', 'lon', 'alt', 'speed']
+			print(keys)
+			i = 0
+			while i < len(keys) :
+				if keys[i] in data:
+					#print ('key found: "' , keys[i], '"\n')
+					line += data[keys[i]]
+				else :
+					failures = failures + 1
+					#print ('key MISSING: "' , keys[i], '"\n')
+					line += 'NULL'
+				if i < len(keys) - 1 :
+					line += ','
+				i = i + 1
+			#line += '\n'
+			if failures <= 6 :
+				print ('[[', time.time() - start, ' of 60 sec]] ', stamp, ',', line)
+				out += stamp
+				out += ','
+				out +=  line
+			else:
+				print ('NO GOOD DATA')			
+	f = open('satdata.csv', 'w+')
+	f.write(out)
+	f.close()
+	
+arduino_log()
+	
+	
 
 	
 	
