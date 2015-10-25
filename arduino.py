dc08f556beb17c034b29ee539cadb42a369b67e1
Alex Campbell <alex@alexthejourno.com>
2015-10-25 00:15:36 -0700

    Updated function names and added compression script

--- arduino.py
+++ arduino.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python2
 
+# This file has functions for interfacing with an Arduino board via USB
+
+# Get our libs
 import serial;
 import binascii;
 import time;
 import io;
 
+# When changing your data being sent by Arduino, be sure to change these headers.
+# This is hacky for now, but is cheaper in terms of memory than JSON, 
+# since Arduino only has so much memory to work with.
+
+
+# Headers for data columns
+PCS_DATA_HEADERS = 'timestamp,temperature,humidity,pressure,pTemperature,accelX,accelY,accelZ,magX,magY,magZ,latitude,longitude,altitude,speed\n'
+
+# Abbreviations for headers
+PCS_DATA_KEYS = ['t', 'h', 'p', 'pt', 'ax', 'ay', 'az', 'mx', 'my', 'mz', 'lat', 'lon', 'alt', 'speed']
+
 # Check device TTY and baud rate before deployment to ensure match with Arduino connection
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 
-def read_arduino():
+# Listen to Arduino and get data from sensors
+def pcs_getdata():
 	try: 
 		str = arduino.readline()
 		pairs = str.decode('ascii').split(', ')
@@ -24,43 +39,48 @@ def read_arduino():
 		else :
 			data[item[0]] = item[1]
 		i = i + 1
-	return data
-	
-#def write_arduino(cmdstr)
-#	arduino.write(cmdstr)
-	
+	return data	
 
-#Test output
-def arduino_test():
+# Test output
+def pcs_arduino_test():
 	while True:
-		data = read_arduino()
+		data = pcs_getdata()
 		if data != False :
 			for k,v in data.items():
 				print (k, " -- ", v)
 				
-def arduino_log():
-	out = 'timestamp,temperature,humidity,pressure,pTemperature,accelX,accelY,accelZ,magX,magY,magZ,latitude,longitude,altitude,speed\n'
+# Use for filling rows with NULL data
+def pcs_fillnull():
+	i = 0
+	out = ''
+	while i < len(PCS_DATA_HEADERS.split(',')) :
+		out += 'NULL'
+		if i < len(PCS_DATA_HEADERS.split(',')) - 1 :
+			out += ','
+		i = i + 1
+	return out
+	
+		
+# Logs sensor data and outputs to CSV
+def pcs_logdatacsv():
+	out = PCS_DATA_HEADERS
 	start = time.time()
 	while time.time() - start < 60 :
 		failures = 0
 		line = ''
-		data = read_arduino()
+		data = pcs_getdata()
 		stamp = time.ctime()
 		if data == False :
-			line += 'NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL'
+			line += pcs_fillnull()
 		else :
-			keys = ['t', 'h', 'p', 'pt', 'ax', 'ay', 'az', 'mx', 'my', 'mz', 'lat', 'lon', 'alt', 'speed']
-			print(keys)
 			i = 0
-			while i < len(keys) :
-				if keys[i] in data:
-					#print ('key found: "' , keys[i], '"\n')
-					line += data[keys[i]]
+			while i < len(PCS_DATA_KEYS) :
+				if PCS_DATA_KEYS[i] in data:
+					line += data[PCS_DATA_KEYS[i]]
 				else :
 					failures = failures + 1
-					#print ('key MISSING: "' , keys[i], '"\n')
 					line += 'NULL'
-				if i < len(keys) - 1 :
+				if i < len(PCS_DATA_KEYS) - 1 :
 					line += ','
 				i = i + 1
 			#line += '\n'
@@ -74,8 +94,6 @@ def arduino_log():
 	f = open('satdata.csv', 'w+')
 	f.write(out)
 	f.close()
-	
-arduino_log()
 	
 	
 
---
+++ compresslog.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Compress the data file output by pcs_logdatacsv
+xz satdata.csv
